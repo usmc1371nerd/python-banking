cc702cee24f5e5e5e6a3fd04db59d420642fe458
usmc1371nerd <james.p.dumas@gmail.com>
2024-02-14 19:37:58 -0600

    create class bank account and deposit method

---
+++ main.py
@@ -0,0 +1,10 @@
+class BankAccount:
+    def __init__(self, name, balance=0):
+        self.name = name
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"Depositied {amount} dollars. New balance: {self.balance} dollars.")    
+
+
