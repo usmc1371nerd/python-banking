395779c9afd9db2fbfbf9c374cfdcb971debb6b1
usmc1371nerd <james.p.dumas@gmail.com>
2024-02-20 06:22:17 -0600

    setting up db added for class

--- models/models.py
+++ models/models.py
@@ -1,4 +1,24 @@
-# def checking_account:
+from flask import Flask
+from flask_sqlalchemy import SQLalchemy
+import random
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bank.db'
+db = SQLalchemy(app)
+
+'''The backref parameter creates a reverse relationship between the User model and the CheckingAccount and SavingsAccount models. 
+This means that, in addition to being able to access the user associated with a checking or savings account, you can also access the 
+checking and savings accounts associated with a user directly from the User model.'''
+
+class user:
+
+    id= db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+    checking_accounts = db.relationship('CheckingAccount', backref='user', lazy=True)
+
+#def checking_account:
 # id(random number 9 digit generated)
 # username(minium character of 7 or 8)
 
